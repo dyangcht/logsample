1d9928c53af65a6893f9fc843af4fca9ae841d4e
David <dyangcht@aim.com>
2021-01-08 16:55:34 +0800

    first commit

---
+++ src/main/java/com/example/logs/Controller.java
@@ -0,0 +1,17 @@
+package com.example.logs;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Service greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Service(counter.incrementAndGet(), String.format(template, name));
+	}
+}
