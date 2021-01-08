d76bceba1a47f27b9ccd484307e818e380208fe5
David <dyangcht@aim.com>
2021-01-08 20:21:52 +0800

    add logger

--- src/main/java/com/example/logs/Controller.java
+++ src/main/java/com/example/logs/Controller.java
@@ -1,6 +1,7 @@
 package com.example.logs;
 import java.util.concurrent.atomic.AtomicLong;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	private static final String template = "您好, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
 	@GetMapping("/greeting")
 	public Service greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.out.println("Testing: " + String.format(template, name));
-		System.out.println("除錯內容: " + String.format(template, name));
+		logger.info("Testing: " + String.format(template, name));
+		logger.info("除錯內容: " + String.format(template, name));
 		return new Service(counter.incrementAndGet(), String.format(template, name));
 	}
 }
